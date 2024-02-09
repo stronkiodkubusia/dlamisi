8b943824421c7095d8f6dc27fe096d4ba66d0659
stronkiodkubusia <62671342+stronkiodkubusia@users.noreply.github.com>
2024-02-09 12:04:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var takButton = document.getElementById("tak");
-    var nieButton = document.getElementById("nie");
+    var takBtn = document.getElementById("tak");
+    var nieBtn = document.getElementById("nie");
     var pytDiv = document.querySelector(".pyt");
     var container = document.querySelector(".container");
-    var audio = new Audio("happy.mp3"); 
+    var audio = new Audio("happy.mp3");
 
+    function gifPoBtn() {
 
-    function handleButtonClick() {
-   
-        if (!document.querySelector(".gif-image")) {
-       
-            var gifImage = document.createElement("img");
-            gifImage.src = "miski.gif"; 
-            gifImage.classList.add("gif-image");
+        if (!document.querySelector(".gifMis")) {
 
-     
-            gifImage.style.position = "absolute";
-            gifImage.style.top = "50%"; 
-            gifImage.style.left = "50%"; 
-            gifImage.style.transform = "translate(-50%, -50%)";
+            var gifMis = document.createElement("img");
+            gifMis.src = "miski.gif";
+            gifMis.classList.add("gifMis");
 
-    
-            container.insertBefore(gifImage, container.firstChild);
+            gifMis.style.position = "absolute";
+            gifMis.style.top = "50%";
+            gifMis.style.left = "50%";
+            gifMis.style.transform = "translate(-50%, -50%)";
 
-          
+            container.insertBefore(gifMis, container.firstChild);
+
             audio.play();
         }
 
-   
-        takButton.style.display = "none";
-        nieButton.style.display = "none";
+        takBtn.style.display = "none";
+        nieBtn.style.display = "none";
         pytDiv.style.display = "none";
     }
 
+    takBtn.addEventListener("click", gifPoBtn);
+    nieBtn.addEventListener("click", gifPoBtn);
 
-    takButton.addEventListener("click", handleButtonClick);
-    nieButton.addEventListener("click", handleButtonClick);
-
-
-    nieButton.addEventListener("mouseover", function () {
-        nieButton.classList.remove("btn2");
-        nieButton.classList.add("btn");
-        nieButton.innerHTML = "tak";
+    nieBtn.addEventListener("mouseover", function () {
+        nieBtn.classList.remove("btn2");
+        nieBtn.classList.add("btn");
+        nieBtn.innerHTML = "tak";
     });
 
-    nieButton.addEventListener("mouseout", function () {
-        nieButton.classList.remove("btn");
-        nieButton.classList.add("btn2");
-        nieButton.innerHTML = "nie";
+    nieBtn.addEventListener("mouseout", function () {
+        nieBtn.classList.remove("btn");
+        nieBtn.classList.add("btn2");
+        nieBtn.innerHTML = "nie";
     });
 });
